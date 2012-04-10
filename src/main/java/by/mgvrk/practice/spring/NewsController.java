d953fa0139877da5cdf4d9f7988822d7c2385443
Sergey Haritonchik <berreig@gmail.com>
2012-04-10 13:27:28 +0300

    spring controller with annotations

---
+++ src/main/java/by/mgvrk/practice/spring/NewsController.java
@@ -0,0 +1,39 @@
+package by.mgvrk.practice.spring;
+
+import by.mgvrk.practice.service.RssService;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedOutput;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: sharitonchik
+ */
+@Controller
+@RequestMapping("/rss")
+public class NewsController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public void getHandler(@RequestParam("news") String news, HttpServletResponse resp) {
+        RssService rssService = new RssService();
+        SyndFeedOutput out = new SyndFeedOutput();
+        SyndFeed feeds = rssService.getSyndFeed(news);
+
+        resp.setContentType("text/xml; charset=utf-8");
+
+        try {
+            out.output(feeds, resp.getWriter());
+        } catch (FeedException e) {
+            e.printStackTrace(System.out);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+
+    }
+
+}
